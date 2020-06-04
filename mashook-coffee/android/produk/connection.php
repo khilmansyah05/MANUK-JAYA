0b1c4fd0f1deb01927576f9d35fd976cbb56f100
mahezabr <mahezabr99@gmail.com>
2020-06-04 09:49:06 +0700

    revisi

--- mashook-coffee/android/produk/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOSTNAME', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DB_SELECT', 'kopi');
-$koneksi = new
-mysqli(HOSTNAME,USERNAME,PASSWORD,DB_SELECT) or die
-(mysqli_errno());
-?>
